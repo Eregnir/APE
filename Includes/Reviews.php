481a524b1b77153e71939952ca0aab716f6497cf
Assaf Zick <zickmusic@gmail.com>
2021-02-04 13:44:14 +0200

    our first push together

--- Includes/Reviews.php
+++ Includes/Reviews.php
@@ -1,64 +1,127 @@
 <html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/audioplayer.css" />
+    <script src="jquery.js"></script>
+    <script src="audioplayer.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nova+Flat&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="../CSS/APEStyle.css">
+    
+    
 </head>
-<!DOCTYPE html>
-<body>
+    <body>
+        <div id="headerph"></div>
+        <div>
 
-<?php
-$servername = "localhost";
-$username = "assafye";
-$password = "1forall!";
-$dbname = "assafye_ApeGaming";
+        <?php
+        
+            $servername = "localhost";
+            $username = "assafye";
+            $password = "1forall!";
+            $dbname = "assafye_ApeGaming";
+            
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                 die("Connection failed: " . $conn->connect_error);
+            } 
+        
+            $sql = "SELECT review_content, game_UID, rating, rating_user, review_title FROM reviews";
+            $result = $conn->query($sql); ?>
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-     die("Connection failed: " . $conn->connect_error);
-} 
+            <!--Reviews Intro-->
+            <div class="container container2_2">
+                <center>
+                    <h1>Game Reviews</h1>
+                    <h3>The biggest reviews library in the world.<br> it will be a blast!</h3>
+                    <img src ="../Images/mario.gif" style="width: 40%;">
+                </center>            
+            </div>
+            <div class="container container2 container4">
+                <br>
+                <div class="container6 row" id="single podcast">
+                    <div class="btn col-sm-2" style="margin: auto; cursor:alias;">
+                        <img id="pd1" class="center center5" src="../images/Abba.png" style=" width: 150px; border-radius: 50%;">   
+                    </div>
+                    <div class="col-lg-10">
+                        <h3>Gaming with APE - Assaf, Pearl and Elor discuss hookers and drugs</h3>
+                        <br><audio class="audio" src="../Audio/PC1.mp3" preload="auto" controls></audio><br>
+                        <p><b>Description:</b><br>
+                        Lorem Ipsum dolor sit amet</p>
+                    </div>
+                </div>
+                <br>
+                <div class="container6 row" id="single podcast">
+                    <div class="col-sm-2" style="margin: auto;">
+                        <img class="center center5" src="../images/Abba.png" style="width: 150px; border-radius: 50%;">
+                    </div>
+                    <div class="col-lg-10">
+                        <h3>Gaming with APE - Pearl solo masturbation on latest NBA, featuring Dani Avdja</h3>
+                        <br><audio class="audio" src="../Audio/PC1.mp3" preload="auto" controls></audio><br>
+                        <p><b>Description:</b><br>
+                        Lorem Ipsum dolor sit amet</p>
+                    </div>
+                </div>
+            </div>
 
-$sql = "SELECT review_content, game_UID, rating, rating_user, review_title FROM reviews";
-$result = $conn->query($sql);
+        </div>
 
-if ($result->num_rows > 0) {
-     echo "<style>table{  text-align:center; background-color:yellow; border: 2px blue solid;} tr{ color: red; border: 2px blue solid;} td{ color: red; border: 2px blue solid;}</style><table><tr><th>ID</th><th>Name</th><th>Age</th>";
-     // output data of each row
-     while($row = $result->fetch_assoc()) {
-         echo "<tr><td>" . $row["review_title"]. "</td><td>" . $row["game_UID"]. "</td><td>" . $row["rating"]. "</td></tr>";
-     }
-     echo "</table>";
-} else {
-     echo "0 results";
-}
+        
+            
+            <!-- <table>
+                <tr>
+                    <td>Title</td>
+                    <td>Rating</td>
+                    <td>User</td>
+                    <td>Game ID</td>
+                    <td>The Review</td>
+                </tr> -->
+        
+            <?php foreach ($result as $review): ?>
+                    <div class="container container2 container4 row" style="background color: blue;">
+                        <div class="col-md-6">
+                        <?php echo $review["review_title"]; ?>
+                        </div>
+                        <div class="col-md-6">
+                        <?php echo $review["review_content"]; ?>
+                        </div>
+                    </div>
+                <p>This is a joke</p>
+                
+            <?php endforeach; ?>
+        
+        
+            
+            
+            <!-- while($row = $result->fetch_assoc()) {
+                echo $row["review_title"];
+                echo ("<br>");
+                echo $row["rating"];
+                echo ("<br>");
+                echo $row["rating_user"];
+                echo ("<br>");
+                echo $row["game_UID"];
+                echo ("<br>");
+                echo $row["review_content"];
+                echo ("<br>");
+        
+            } -->
+        <!-- ?> -->
+        <div id="footerph"></div>
 
-$iid=$_POST["Title"];
-$name=$_POST["Game ID"];
-$age=$_POST["Rating"];
+        <script type="text/javascript">
+        $(function(){
+            $("#headerph").load("header.html");
+        });
+        $(function(){
+            $("#footerph").load("footer.html");
+        });
+        </script>
 
-// $sql2="INSERT INTO users (id, name, age) VALUES ('".$iid."','".$name."','".$age."');";
-// $sql3="INSERT INTO users (id, name, age) VALUES ('2299', 'Miko Levinsky', 32);";
-// $conn->query($sql2);
-// $conn->query($sql3);
-// $result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    echo "<style>table{  text-align:center; background-color:yellow; border: 2px blue solid;} tr{ color: red; border: 2px blue solid;} td{ color: red; border: 2px blue solid;}</style><table><tr><th>ID</th><th>Name</th><th>Age</th>";
-     // output data of each row
-     while($row = $result->fetch_assoc()) {
-         echo "<tr><td>" . $row["id"]. "</td><td>" . $row["name"]. "</td><td> " . $row["age"]. "</td></tr>";
-     }
-     echo "</table>";
-} else {
-     echo "0 results";
-}
-$conn->close();
-?>  
-
-
-<br>
-</body>
+    </body>
 </html>
